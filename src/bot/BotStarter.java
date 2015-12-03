338411148b604499218e535bcc5a386e0cc11cd3
Servaas Tilkin <servaas.tilkin@gmail.com>
2015-12-03 23:54:22 +0100

    Fixed bug in recursive algorithm

--- src/bot/BotStarter.java
+++ src/bot/BotStarter.java
@@ -17,11 +17,7 @@
 
 package bot;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
 
 /**
  * BotStarter class
@@ -34,13 +30,11 @@ import java.util.Random;
 
 public class BotStarter {
     public static final HashMap<String, Integer> memoize = new HashMap<String, Integer>();
-    public static final int WE_WIN = 1000;
-    public static final int WE_DRAW = 100;
-    public static final int WE_LOSE = -1000;
-    public static final int NONE = -999;
+    public static final int WINNING = 1000;
+    public static final int WE_DRAW = 0;
+    public static final int LOSING = -1000;
     private static final int[] COL_ORDER = { 3, 4, 2, 5, 1, 6, 0 };
     public static BotParser parser;
-    private final Random rand = new Random();
     private Field field;
 
     public void setField(Field field) {
@@ -55,47 +49,62 @@ public class BotStarter {
     public int makeTurn() {
 	final long start = System.currentTimeMillis();
 	final int enemyId = 3 - BotParser.mBotId; // 3-2=1; 3-1=2
+	memoize.clear(); // let me refresh my memory
 
-	int bestVal = WE_LOSE;
-	int bestCol = COL_ORDER[0];
+	int bestVal = LOSING;
+	int bestCol = 0;
 	int currentVal;
 	for (int i = 0; i < COL_ORDER.length; i++) {
 	    if (field.isValidMove(COL_ORDER[i])) {
 		currentVal = getColumnValue(field.toString(), COL_ORDER[i], BotParser.mBotId, enemyId, 8);
+		System.err.println(COL_ORDER[i] + " " + currentVal);
 		if (currentVal > bestVal) { // minimize opponent value
 		    bestVal = currentVal;
 		    bestCol = COL_ORDER[i];
-		    if (bestVal == WE_WIN) {
+		    if (bestVal == WINNING) {
 			break;
+		    } else if (bestVal == WE_DRAW) {
+			final long duration = System.currentTimeMillis() - start;
+			if (duration > 499l) {
+			    System.err.println("This is taking too long...");
+			    break;
+			}
 		    }
+
 		}
 	    }
 	}
 	final long duration = System.currentTimeMillis() - start;
-	System.err.println("Col: " + bestCol + " in " + duration + "ms");
+	System.err.println("Col: " + bestCol + " in " + duration + "ms for outcome " + bestVal);
 
 	return bestCol;
     }
 
     /**
      * Evaluates the value of throwing a coin in a column
-     * @param fieldStr current state of the field (before throwing the coin)
-     * @param column the column you want to throw in
-     * @param player the player you want to get the value for
-     * @param opponent the opponent of the player
-     * @param branch limiting factor, lower means quicker answer, but less in-depth investigation. Suggested value at least 8
+     * 
+     * @param fieldStr
+     *            current state of the field (before throwing the coin)
+     * @param column
+     *            the column you want to throw in
+     * @param player
+     *            the player you want to get the value for
+     * @param opponent
+     *            the opponent of the player
+     * @param branch
+     *            limiting factor, lower means quicker answer, but less in-depth investigation. Suggested value at least 8
      * @return one of the constants WIN LOSE or DRAW
      */
     private int getColumnValue(final String fieldStr, final int column, final int player, final int opponent, final int branch) {
 
 	if (branch <= 0) { // limit branching for time constraint
-	    return WE_LOSE;
+	    return WE_DRAW;
 	}
 
 	final Field newField = new Field(field.getNrColumns(), field.getNrRows());
 	newField.parseFromString(fieldStr);
-	newField.addDisc(column, player);
-	// System.err.println(localField.toPrettyString());
+	newField.addDisc(column, player); // add my coin
+	// System.err.println(newField.toPrettyString()); // uncomment for debug
 
 	// use memorization to quicken the pace
 	final String newFieldString = newField.toString();
@@ -104,44 +113,37 @@ public class BotStarter {
 	    return rememberedAnswer;
 	}
 
+	// easy outcomes
 	if (newField.hasFourInARow(player)) {
-	    memoize.put(newFieldString, WE_WIN);
-	    return WE_WIN;
+	    memoize.put(newFieldString, WINNING);
+	    return WINNING;
 	} else if (newField.hasFourInARow(opponent)) {
-	    memoize.put(newFieldString, WE_LOSE);
-	    return WE_LOSE;
+	    memoize.put(newFieldString, LOSING);
+	    return LOSING;
 	} else if (newField.isFull()) {
 	    memoize.put(newFieldString, WE_DRAW);
 	    return WE_DRAW;
 	}
 
-	int enemyVal = WE_WIN;
-	int currentVal;
+	// guess what opponent will do during his move
+	int enemyVal = LOSING;
+	int currentEnemyVal;
 	for (int i = 0; i < COL_ORDER.length; i++) {
 	    if (newField.isValidMove(COL_ORDER[i])) {
-		// opponents move
-		currentVal = getColumnValue(newFieldString, COL_ORDER[i], opponent, player, branch - 1);
-		if (currentVal < enemyVal) { // minimize opponent value
-		    enemyVal = currentVal;
-		    if (enemyVal == WE_LOSE) {
+		currentEnemyVal = getColumnValue(newFieldString, COL_ORDER[i], opponent, player, branch - 1);
+		if (currentEnemyVal > enemyVal) { // maximize opponent value
+		    enemyVal = currentEnemyVal;
+		    if (enemyVal == WINNING) {
 			break;
 		    }
 		}
 	    }
 	}
 
-	if (enemyVal == WE_LOSE) {
-	    memoize.put(newFieldString, WE_WIN);
-	    return WE_WIN; // opponent loss
-	} else if (enemyVal == WE_WIN) {
-	    memoize.put(newFieldString, WE_LOSE);
-	    return WE_LOSE; // opponent win
-	} else {
-	    memoize.put(newFieldString, WE_DRAW);
-	    return WE_DRAW;
-	}
+	final int ourValue = -enemyVal; // our goal is opposed to that of the opponent
+	memoize.put(newFieldString, ourValue);
+	return ourValue;
     }
-
 
     /**
      * MAIN METHOD
