4932425d6c7ed9a5f531b638363fe012c1fed86b
Servaas Tilkin <servaas.tilkin@gmail.com>
2016-01-24 21:46:41 +0100

    Small patches to eval

--- src/bot/Rating.java
+++ src/bot/Rating.java
@@ -1,6 +1,7 @@
 package bot;
 
 public class Rating {
+    public static final int ALWAYS_WINNING = 9999;
     public long wins = 0;
     public long losses = 0;
     public long draws = 0;
@@ -12,19 +13,22 @@ public class Rating {
     }
 
     public int getValue() {
+	if (losses == 0) {
+	    // "We simply cannot lose" ;-)
+	    return ALWAYS_WINNING;
+	}
+
 	final float winRate = (float) wins / (wins + losses + 1);
-	final float drawRate = (float) (wins + draws) / (wins + draws + losses);
+	final float drawRate = (float) draws / (draws + losses + 1);
 
-	float value = 5 * winRate;
-	value += drawRate;
-
+	float value = 3 * winRate + 2 * drawRate;
 	return (int) (1000 * value);
     }
 
     @Override
     public String toString() {
 	final float winRate = (float) wins / (wins + losses + 1);
-	final float drawRate = (float) (wins + draws) / (wins + draws + losses);
+	final float drawRate = (float) draws / (draws + losses + 1);
 
 	return String.format("%6d wins, %6d draws, %6d losses \t(%.3f rate1, %.3f rate2)", wins, draws, losses, winRate, drawRate);
     }
